11a2fb737c26cb2aba18f1d2673efaa89f65ba64
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-05-21 16:04:32 -0300

    add: Timezone

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pytz
 
 db = SQLAlchemy()
+
+def get_sao_paulo_time():
+    sao_paulo_tz = pytz.timezone("America/Sao_Paulo")
+    return datetime.now(sao_paulo_tz)
 
 class Locomotiva(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,7 +16,7 @@ class Locomotiva(db.Model):
     pressao_oleo_motor = db.Column(db.Float, nullable=False)
     temperatura_combustivel = db.Column(db.Float, nullable=False)
     temperatura_oleo_refrigeracao = db.Column(db.Float, nullable=False)
-    data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
+    data_criacao = db.Column(db.DateTime, default=get_sao_paulo_time)
 
     def __repr__(self):
         return f'<Locomotiva {self.id} - Temperatura Motor: {self.temperatura_motor}°C>'
